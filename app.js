823f491cc808fdec66742d961ee708d288104730
vimal-arockia-raj <vimalarockiaraj9532@gmail.com>
2024-08-29 14:10:40 +0530

    app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import PasswordManager from './components/PasswordManager'
+
+function App() {
+  return (
+    <div className="App">
+      <PasswordManager />
+    </div>
+  )
+}
+
+export default App
+
+
+//infdex.cs//
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
